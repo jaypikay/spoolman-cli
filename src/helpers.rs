700c031a40e84279bd0721cd1f8318e7583892ea
jpk <jpk@goatpr0n.de>
2024-04-29 12:52:37 +0200

    Added Flow rate extra field

--- src/helpers.rs
+++ src/helpers.rs
@@ -15,12 +15,13 @@ fn hex_to_rgb(hex: &str) -> Option<(u8, u8, u8)> {
 
 pub fn print_spool_table_header() {
     println!(
-        "{:^4} {:48} {:9} {:^8} {:4} {:5} {:>9} {:>5} {:>7} {:20}",
+        "{:^4} {:48} {:9} {:^8} {:4} {:5} {:5} {:>9} {:>5} {:>7} {:20}",
         "ID".underline(),
         "Filament".underline(),
         "Material".underline(),
         "Color".underline(),
         "K".underline(),
+        "Flow".underline(),
         "PA".underline(),
         "Remaining".underline(),
         "%".underline(),
@@ -35,17 +36,22 @@ pub fn print_spool_table_row(spool: &spool::Spool) {
         Some(value) => value,
         None => "",
     };
+    let flow_rate = match spool.filament.extra.get("flow_rate") {
+        Some(value) => value,
+        None => "",
+    };
     let k_factor = match spool.extra.get("k_factor") {
         Some(value) => value,
         None => "",
     };
     println!(
-        "{:4} {:48} {:9} {:^8} {:4} {:5} {:>9.2} {:5.1} {:>7.2} {:20}",
+        "{:4} {:48} {:9} {:^8} {:4} {:5} {:5} {:>9.2} {:5.1} {:>7.2} {:20}",
         spool.id,
         spool.filament.name,
         spool.filament.material,
         spool.filament.color_hex.on_truecolor(r, g, b),
         k_factor,
+        flow_rate,
         pressure_advance,
         spool.remaining_weight,
         spool.remaining_weight / spool.filament.weight * 100.,
